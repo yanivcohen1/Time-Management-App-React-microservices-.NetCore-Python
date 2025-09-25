4a9190b3b87f65c6b7befce606c9a6d4a545fe3a
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-09-25 19:10:04 +0300

    ud

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,1 +1,8 @@
 declare module 'react-top-loading-bar';
+declare module '*.css';
+declare module '*.scss';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
