30f5db0d37a418e09e61acc96875b1b02ba97002
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-10-25 18:06:28 +0300

    ud

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Home from './pages/Home/Home';
 import { MemoryRouter } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
@@ -29,6 +29,33 @@ test('renders Home Set AppContext button', () => {
       </AuthProvider>
     </AppProvider>
   );
+  
   const linkElement = screen.getByText(/Set AppContext/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('clicking Set global User to Alice button updates global storage display', () => {
+  render(
+    <AppProvider>
+      <AuthProvider>
+        <ToastProvider>
+          <MemoryRouter>
+            <Home onToggleCookieBanner={() => {}} isCookieBannerVisible={false} />
+          </MemoryRouter>
+        </ToastProvider>
+      </AuthProvider>
+    </AppProvider>
+  );
+
+  // Find the button
+  const button = screen.getByText(/Set global User to Alice/i);
+  // const button = screen.getByTestId('my-button'); <button data-testid="my-button" ...>
+  expect(button).toBeInTheDocument();
+
+  // Click the button
+  fireEvent.click(button);
+
+  // Check that the display text updates
+  const displayText = screen.getByText(/Global Storage: global Alice/i);
+  expect(displayText).toBeInTheDocument();
 });
